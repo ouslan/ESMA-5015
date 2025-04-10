4d069bb840feabd42dbe5649b5e2eee0efeb18c5
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-04-10 10:18:37 -0400

    fixed bug with running mean

--- homeworks/exam-01/code/test.py
+++ homeworks/exam-01/code/test.py
@@ -1,15 +1,11 @@
 # Estimar E[X^2] y construir la gráfica de la convergencia de los "running means"
-running_means = np.cumsum(samples) / np.arange(1, n_samples + 1)
+running_means = np.cumsum(samples) / np.arange(1, n_samples + 1) + (np.cumsum(samples) / np.arange(1, n_samples + 1))**2
 
 # Graficar la convergencia
 plt.plot(running_means, label="Running Mean")
-plt.axhline(y=np.mean(samples), color="r", linestyle="--", label="Media Estimada")
+plt.axhline(y=3.75, color="r", linestyle="--", label="Media Estimada")
 plt.xlabel("Número de Muestras")
 plt.ylabel("Running Mean")
 plt.legend()
 plt.title("Convergencia del Running Mean a la Media Estimada")
 plt.show()
-
-# Estimación de E[X^2]
-E_X2_estimated = np.mean(samples) + np.mean(samples) ** 2
-print("Estimación de E[X^2]:", E_X2_estimated)
